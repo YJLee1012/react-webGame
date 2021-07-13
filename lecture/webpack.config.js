4291db4b0d562ea8917187839a9dc77fe6917a1c
yjlee1012 <elj308@naver.com>
2021-07-13 16:35:43 +0900

    webpack빌드하기

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -9,11 +9,23 @@ module.exports = {
     },
 
     //파일 합치기
+    //client.jsx, WordRelay.jsx 두가지 파일을 합쳐서 app.js파일로 합쳐서 html이 실행할 수 있도록 만들어준다.
     entry: {
         // app: ['./client.jsx', './WordRelay.jsx'],
         //이미 client.jsx파일이 WordRelay.jsx파일 불러오고 있어
         app: ['./client'],
     }, //입력
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader:'babel-loader', //js나jsx파일에 바벨로더를 적용해 최신문법이 옛날 브라우저에서도 돌아갈 수 있도록 해줌
+            options:{
+                presets:['@babel/preset-env','@babel/preset-react'],
+            },
+        }],
+    },
+
     output : {
         path: path.join(__dirname,'dist'), //__dirname은 현재폴더(lecture)
         filename: 'app.js',
